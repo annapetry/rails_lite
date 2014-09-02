f02788d242e6f314ff94247230063898cffe9473
Anna Petry <hello@annapetry.com>
2014-09-02 15:01:47 -0700

    Complete Phase IV. Stores cookies.

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -1,12 +1,22 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      read_template = File.read(
+        "views/#{ self.class.name.underscore }/#{ template_name }.html.erb"
+      )
+
+      new_template = ERB.new(read_template).result(binding)
+
+      render_content(new_template, "text/html")
     end
   end
 end
+
+
