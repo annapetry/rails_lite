42448e266f0037521ea4ecb5ca280c9c29ba71dd
Anna Petry <hello@annapetry.com>
2014-09-02 14:07:02 -0700

    Complete Phase III. Add render() to Controllerbase.

--- skeleton/lib/phase2/controller_base.rb
+++ skeleton/lib/phase2/controller_base.rb
@@ -4,20 +4,39 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response == res
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      res.status = 302
+      res["location"] = url
+      if already_built_response?
+        raise Exception
+      else
+        @already_built_response = res
+      end
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, type)
+      res.content_type = type
+      res.body = content
+      if already_built_response?
+        raise Exception
+      else
+        @already_built_response = res
+      end
     end
   end
 end
+
+
